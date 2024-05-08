ca67a61e568072f564dd00ad7a6b4ae00b2536df
Kshitij1997 <31150336+Kshitij1997@users.noreply.github.com>
2024-05-07 22:03:46 -0400

    KSB-01 First commit for spring starter project

---
+++ shiny/src/main/java/com/octo/shiny/ShinyApplication.java
@@ -0,0 +1,13 @@
+package com.octo.shiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinyApplication.class, args);
+	}
+
+}
